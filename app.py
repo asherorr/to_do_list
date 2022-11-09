051e51c7884d7b62953bac087ac4cbd42e11a6cf
asherorr <asher@mailfence.com>
2022-11-09 18:25:17 -0500

    This commit will add any submissions to the database.

--- app.py
+++ app.py
@@ -1,24 +1,39 @@
-from flask import Flask, render_template, url_for
-from models import db, to_do_list, app
+from flask import Flask, render_template, url_for, request, redirect
+from models import db, Todo, app
+import datetime
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
 
-@app.route('/add_to_list')
+@app.route('/add_to_list', methods=['GET', 'POST'])
 def add_to_list():
-    return render_template('addtolist.html')
+    the_list = Todo.query.all()
+    if request.form:
+        split_date = request.form['date'].split('-')
+        year = int(split_date[0])
+        month = int(split_date[1])
+        day = int(split_date[2])
+        date_to_submit = datetime.date(year, month, day)
+        new_addition = Todo(title=request.form['name'], date_created=date_to_submit)
+        db.session.add(new_addition)
+        db.session.commit()
+        return redirect(url_for('index'))
+    return render_template('addtolist.html', the_list=the_list)
 
-
-@app.route('/view_list')
-def view_list():
-    pass
 
 
 @app.route('/edit_list')
 def edit_list():
     pass
+
+
+@app.route('/view_list')
+def view_list():
+    to_do_list = Todo.query.all
+    return render_template('showlist.html', to_do_list=to_do_list)
+    
     
     
 if __name__ == '__main__':
--- models.py
+++ models.py
@@ -6,7 +6,8 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///to-do.db'
 db = SQLAlchemy(app)
 
-class to_do_list(db.Model):
+class Todo(db.Model):
     id= db.Column(db.Integer, primary_key=True)
-    created = db.Column('Created', db.DateTime, default=datetime.datetime.now)
-    name = db.Column(db.String())
+    title = db.Column('Title', db.String())
+    date_created = db.Column('Created', db.Date)
+    
