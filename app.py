9d69f55d656e701993ff660f86482776c75b2139
asherorr <asher@mailfence.com>
2022-11-17 20:17:24 -0500

    This commit will display the list items on the showlist.html page.

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, url_for, request, redirect
-from models import db, Todo, app
+from models import db, Item, app
 import datetime
 
 @app.route('/')
@@ -9,31 +9,45 @@ def index():
 
 @app.route('/add_to_list', methods=['GET', 'POST'])
 def add_to_list():
-    the_list = Todo.query.all()
+    list_of_items = Item.query.all()
     if request.form:
         split_date = request.form['date'].split('-')
         year = int(split_date[0])
         month = int(split_date[1])
         day = int(split_date[2])
         date_to_submit = datetime.date(year, month, day)
-        new_addition = Todo(title=request.form['name'], date_created=date_to_submit)
+        new_addition = Item(title=request.form['name'], date_created=date_to_submit)
         db.session.add(new_addition)
         db.session.commit()
         return redirect(url_for('index'))
-    return render_template('addtolist.html', the_list=the_list)
-
-
-
-@app.route('/edit_list')
-def edit_list():
-    pass
+    return render_template('addtolist.html', list_of_items=list_of_items)
 
 
 @app.route('/view_list')
 def view_list():
-    to_do_list = Todo.query.all
-    return render_template('showlist.html', to_do_list=to_do_list)
-    
+    the_list = Item.query.all()
+    return render_template('showlist.html', the_list=the_list)
+
+
+
+# @app.route('/list/<id>/edit', methods=['GET', 'POST'])
+# def edit_list_item(id):
+#     list_items = Item.query.all()
+#     item = Item.query.get_or_404(id)
+#     if request.form:
+#         split_date = request.form['date'].split('-')
+#         year = int(split_date[0])
+#         month = int(split_date[1])
+#         day = int(split_date[2])
+#         date_to_submit = datetime.date(year, month, day)
+#         item.title = request.form['title']
+#         item.date_created = date_to_submit
+#         db.session.commit()
+#         return redirect(url_for('index'))
+#     return render_template('editlist.html', item=item, list_items=list_items)
+
+
+
     
     
 if __name__ == '__main__':
--- models.py
+++ models.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///to-do.db'
 db = SQLAlchemy(app)
 
-class Todo(db.Model):
+class Item(db.Model):
     id= db.Column(db.Integer, primary_key=True)
     title = db.Column('Title', db.String())
     date_created = db.Column('Created', db.Date)
